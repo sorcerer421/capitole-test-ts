8214d77caa5f83eaa00feecbf1b46e1c035bf673
Alfonso Betancourt <sorcerer421@users.noreply.github.com>
2024-04-25 18:44:57 +0200

    Update index.tsx

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import PodcastCard from '@/components/parts/Shared/PodcastCard';
 import { usePodcast } from '@/query/podcast';
 import { Podcast, ReceivedPodcast } from '@/repositories/podcast/types';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const Home = () => {
   const router = useRouter();
@@ -20,17 +21,29 @@ const Home = () => {
       author: podcast['im:artist'].label
   })
   });
+  const [filteredList, setFilteredList] = useState<Podcast[]>(actualList);
 
   const handleClick = (id: string) => {
     router.push(`/podcast/${id}`);
   }
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const copyActualList = actualList.filter((podcast: { title: string | string[]; }) => podcast.title.includes(value));
+    setFilteredList(copyActualList);
+  }
+
   return (
     <>
       <Header loading={podcastQuery.isFetching}/>
-      <div className='pl-48 bg-gray-200'>
+      <div className='flex justify-end w-full bg-gray-100 p-4'>
+        <div className='w-64'>
+          <input type="text" placeholder="Search" onChange={(event)=> handleSearch(event)}/>
+        </div>  
+      </div>
+      <div className='pl-48 bg-gray-100'>
         <div className='flex flex-wrap'>
-        {actualList?.map((podcast) => <PodcastCard key={podcast.id} {...podcast} handleClick={handleClick}/>)}
+        {filteredList?.map((podcast) => <PodcastCard key={podcast.id} {...podcast} handleClick={handleClick}/>)}
         </div>
       </div>
     </>
